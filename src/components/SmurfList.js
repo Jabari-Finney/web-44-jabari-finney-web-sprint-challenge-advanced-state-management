f21db401869735dd81966a329213c734ccf1c8f1
Jabari-Finney <jabaritf@gmail.com>
2021-07-16 19:48:42 -0400

    npm issues resolved

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -3,21 +3,13 @@ import Smurf from './Smurf';
 import { connect } from 'react-redux'
 
  const SmurfList = ( props )=> {
-   /*  const isLoading = false;
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.' */
-    
-
+   
     if (props.isLoading) {
         return <h1>Loading...</h1>;
     }
 
     return(<div className="listContainer">
-        {props.smurf.map(smurf => {
+        {props.smurfs.map(smurf => {
             return <Smurf key={smurf.id} smurf={smurf}/>
         })}
     </div>);
